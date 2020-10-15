f6d8825eb2dfd9dfcb4e90d9b2d65cfd4f3743f5
camboya <64973355+camboya@users.noreply.github.com>
2020-10-15 10:47:01 -0300

    Update ejemplos_clase.py

--- ejemplos_clase.py
+++ ejemplos_clase.py
@@ -4,7 +4,7 @@ JSON XML [Python]
 Ejemplos de clase
 ---------------------------
 Autor: Inove Coding School
-Version: 1.3
+Version: 1.2
 
 Descripcion:
 Programa creado para mostrar ejemplos prácticos de los visto durante la clase
